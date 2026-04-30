41156d0036b7938c75ef07e0de19aa5b24137239
Pedro-Pipponzi <pedropsiqueira2007@gmail.com>
2026-04-30 10:04:03 -0300

    Questão 4

--- funcoes.py
+++ funcoes.py
@@ -23,3 +23,13 @@ def remover_dado(dados_rolados, dados_estoque, remover):
             novo_estoque.append(dados_estoque[i])
     dados_rolados.append(dado)
     return [dados_rolados, novo_estoque]
+
+def calcula_pontos_regra_simples(lista):
+    dicio = {}
+    for face in range(1, 7):
+        pontos = 0
+        for dado in lista:
+            if dado == face:
+                pontos += face
+        dicio[face] = pontos
+    return dicio
