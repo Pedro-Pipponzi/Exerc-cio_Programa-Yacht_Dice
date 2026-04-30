b0700fb7aba7745fe6cc01b9f26764623780531e
ricardocrestana <ricardouc1@al.insper.edu.br>
2026-04-30 14:05:28 -0300

    Questão 6

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,13 @@
 import random
 
+#1
 def rolar_dados(n):
     dados = []
     for _ in range(n):
         dados.append(random.randint(1, 6))
     return dados
 
+#2
 def guardar_dado(rolados, estoque, guardar):
     dado = rolados[guardar]
     novorolados = []
@@ -15,6 +17,7 @@ def guardar_dado(rolados, estoque, guardar):
     estoque.append(dado)
     return [novorolados, estoque]
 
+#3
 def remover_dado(dados_rolados, dados_estoque, remover):
     dado = dados_estoque[remover]
     novo_estoque = []
@@ -24,6 +27,7 @@ def remover_dado(dados_rolados, dados_estoque, remover):
     dados_rolados.append(dado)
     return [dados_rolados, novo_estoque]
 
+#4
 def calcula_pontos_regra_simples(lista):
     dicio = {}
     for face in range(1, 7):
@@ -34,8 +38,23 @@ def calcula_pontos_regra_simples(lista):
         dicio[face] = pontos
     return dicio
 
+#5
 def calcula_pontos_soma(lista):
     pontuacao = 0
     for dado in lista:
         pontuacao += dado
     return pontuacao
+
+#6
+def calcula_pontos_sequencia_baixa(lista):
+    lista2 = sorted(set(lista))
+    i = 0
+    for j in range(1, len(lista2)):
+        if lista2[j] == lista2[j-1] + 1:
+            i += 1
+            if i >= 3:
+                return 15
+        else:
+            i = 0
+    
+    return 0
